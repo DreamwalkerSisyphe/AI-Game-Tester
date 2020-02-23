63cbc4af71de3a55b1363ba54a8bb6d8fee2a28b
cruzad <38330475+cruzad@users.noreply.github.com>
2020-02-22 18:42:46 -0800

    Add files via upload

---
+++ converter.cpp
@@ -0,0 +1,17 @@
+#include "scenario.cpp"
+#include <iostream> 
+#include <iomanip>
+#include <string>
+#include <fstream>
+
+ifstream inputSpell, inputScenario;
+
+void readSpell()
+{
+
+}
+
+void readScenario()
+{
+
+}
